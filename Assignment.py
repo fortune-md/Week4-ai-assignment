1d10ab94f8ae8e45f748f442588538a29bd27ec5
fortune-md <fortu7792@gmail.com>
2026-03-11 20:38:27 +0300

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,1 +1,31 @@
+# 4.python program that print hello Ai student
+# print("Hello AI Students")
 
+# 5.create variables with Name, Age, City
+#name = "John"
+age = 20
+city = "Nairobi"
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+# 6.Create a list with following peogramming language(python,java,c++,Go
+# languages = ["Python", "Java", "C++", "JavaScript", "Go"]
+print(languages[0])  
+# Prints the first language
+
+# 6.Create dictionary of:Name, age, course, and print student name
+# student = {
+    "name": "John",
+    "age": 21,
+    "course": "AI"
+}
+
+print(student["name"])  
+
+# 7.: Difference between a list and a dictionary
+List: Ordered collection of items. Items are accessed by their position (index).
+Example: languages = ["Python", "Java", "C++"]
+Dictionary: Collection of key-value pairs. Items are accessed using keys instead of indices.
+Example: student = {"name": "John", "age": 21}
